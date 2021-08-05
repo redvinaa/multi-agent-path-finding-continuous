2dfe97e37cbaae8197cab36230e71ccf4b06be41
redvinaa <redvinaa@gmail.com>
2021-08-05 16:12:33 +0200

    Delete devel folder from repo

--- devel/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/vince/study/multi-agent-path-planning/code/multiagent-path-finding-continuous/devel}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
