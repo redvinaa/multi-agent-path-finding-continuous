00ebf0ec4dcbd872c8a9cc000162b062a42b0bd5
redvinaa <redvinaa@gmail.com>
2021-08-10 16:23:38 +0200

    Modify .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -62,16 +62,16 @@ default_flags = [
     #  '-Werror',
     #  '-Wc++98-compat',
     #  '-Wno-long-long',
-    '-Wno-variadic-macros',
-    '-fexceptions',
-    '-DNDEBUG',
+    #  '-Wno-variadic-macros',
+    #  '-fexceptions',
+    #  '-DNDEBUG',
     # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know
     # which language to use when compiling headers. So it will guess. Badly. So
     # C++ headers will be compiled as C headers. You don't want that so ALWAYS
     # specify a "-std=<something>".
     # For a C project, you would set this to something like 'c99' instead of
     # 'c++11'.
-    '-std=c++11',
+    '-std=c++14',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c++ headers.
@@ -82,8 +82,10 @@ default_flags = [
     '.',
 
     # include third party libraries
-    # '-isystem',
-    # '/some/path/include',
+	'-isystem',
+	os.path.expandvars('$ROS_WORKSPACE') + '/src/libtorch/include/torch/csrc/api/include',
+	'-isystem',
+	'/usr/local/include/opencv4',
 ]
 
 flags = default_flags + GetRosIncludeFlags()
