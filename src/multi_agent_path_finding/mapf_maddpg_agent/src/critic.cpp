cf8580c8105bb004462651c4bd6f371d154aa9a7
redvinaa <redvinaa@gmail.com>
2021-08-12 17:16:23 +0200

    Fixup

--- src/multi_agent_path_finding/mapf_maddpg_agent/src/critic.cpp
+++ src/multi_agent_path_finding/mapf_maddpg_agent/src/critic.cpp
@@ -4,19 +4,17 @@
 #include <torch/torch.h>
 
 Critic::Critic(int _number_of_agents, Net* _net, torch::optim::Optimizer* _optim):
-	number_of_agents(_number_of_agents), net(_net)
-{
-    
-}
+    number_of_agents(_number_of_agents), net(_net) {}
 
-Value get_value(CollectiveObservation obs)
+Value Critic::get_value(CollectiveObservation obs)
 {
     Value value;
     return value; // TODO
 }
 
-void train(std::vector<Experience> experiences, std::vector<Value> values)
+std::vector<Value> Critic::train(std::vector<Experience> experiences)
 {
-    
+    std::vector<Value> values;
+    return values; // TODO
 }
 
