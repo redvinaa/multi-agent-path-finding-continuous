ded32ed91fc1cb93a4dafc2819d6918e58b3bea9
redvinaa <redvinaa@gmail.com>
2021-09-08 14:48:48 +0200

    Delete actor_critic packages except MAAC

--- ma_soft_actor_critic/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['ma_soft_actor_critic'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
