34611f75ce01cef6b97c522d48ce0968296a6a2e
redvinaa <redvinaa@gmail.com>
2021-09-15 15:30:45 +0200

    Fix test for mapf_environment_py
    Because tests require python3, they need to be run
    by hand as executables

--- mapf_environment_py/test/test_python_wrapper.py
+++ mapf_environment_py/test/test_python_wrapper.py
@@ -6,58 +6,35 @@ import os
 import sys
 import unittest
 import rospkg
-from mapf_env import Environment as Env
-from mapf_env import Point
+from mapf_env import Environment
 import rosunit
+import numpy as np
 
 __author__ = "Reda Vince"
 __copyright__ = "Copyright 2021 Reda Vince"
 
-pkg = 'mapf_environment'
-
-
-def make_env():
-    pkg_path = rospkg.RosPack().get_path(pkg)
-    img_path = os.path.join(pkg_path, 'maps', 'test_4x4.jpg')
-
-    return Env(img_path, 2)
-
 
 class TestWrapper(unittest.TestCase):
 
-    def test_point(self):
-        p = Point()
-        self.assertEqual(p.x, 0)
-        self.assertEqual(p.y, 0)
-        self.assertEqual(p.z, 0)
-        p.x = 1
-        p.y = 0.5
-        p.z = -3
-        self.assertEqual(p.x, 1)
-        self.assertEqual(p.y, 0.5)
-        self.assertEqual(p.z, -3)
-
     def test_env(self):
-
         # constructor
-        env = make_env()
+        pkg_path = rospkg.RosPack().get_path('mapf_environment')
+        image = os.path.join(pkg_path, 'maps', 'test_4x4.jpg')
+        env = Environment(image, 2)
 
         # reset
         env_obs = env.reset()
         self.assertFalse(env.is_done())
 
         # render && step
-        from mapf_env import Point
-        while not env_obs.done:
+        done = False
+        while not done:
+            act = np.array([[1., -0.5], [0.1, 0.0]])
+            obs, rew, d = env.step(act)
+            done = d[0]
             env.render(10)
-            p1 = Point()
-            p1.x = 1.
-            p2 = Point()
-            p2.x = 1.
-            p2.z = -0.5
-            env_obs = env.step([p1, p2])
 
-        self.assertEqual(env.get_observation_size(), 17)
+        self.assertEqual(env.get_observation_space()[0], 17)
 
 if __name__ == '__main__':
-    rosunit.unitrun(pkg, 'test_wrapper', TestWrapper)
+    rosunit.unitrun('mapf_environment_py', 'test_wrapper', TestWrapper)
