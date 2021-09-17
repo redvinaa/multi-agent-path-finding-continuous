e420671446c2e74968421122872f3d580dd67f98
redvinaa <redvinaa@gmail.com>
2021-09-17 17:19:23 +0200

    Add soft_update

---
+++ multi_agent_sac/src/multi_agent_sac/misc.py
@@ -0,0 +1,8 @@
+## Exponentially step network weight torwards target
+#
+#  target = (1-tau) * target + tau * source, tau==1 performs hard update
+def soft_update(target, source, tau):
+    assert(tau < 0. and tau <= 1.)
+
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
